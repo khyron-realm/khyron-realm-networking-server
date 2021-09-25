0fcdb73cefd3210044b8996e1f04b6361584f91b
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-09-25 22:32:14 +0300

    Added serialization

--- Unlimited-NetworkingServer-MiningGame/Player.cs
+++ Unlimited-NetworkingServer-MiningGame/Player.cs
@@ -1,6 +1,8 @@
+using DarkRift;
+
 namespace Unlimited_NetworkingServer_MiningGame
 {
-    public class Player
+    public class Player: IDarkRiftSerializable
     {
         public string id { get; set; }
         public string name { get; set; }
@@ -17,6 +19,24 @@ namespace Unlimited_NetworkingServer_MiningGame
             this.level = level;
             this.experience = experience;
             this.energy = energy;
+        }
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            id = e.Reader.ReadString();
+            name = e.Reader.ReadString();
+            level = e.Reader.ReadUInt16();
+            experience = e.Reader.ReadUInt16();
+            energy = e.Reader.ReadUInt16();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(id);
+            e.Writer.Write(name);
+            e.Writer.Write(level);
+            e.Writer.Write(experience);
+            e.Writer.Write(energy);
         }
     }
 }
