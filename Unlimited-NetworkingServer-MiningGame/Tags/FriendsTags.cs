df4f9066d48e9f68ec4a89eba46b6ef09d0f9488
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-10-30 20:09:54 +0300

    Added friends tags

---
+++ Unlimited-NetworkingServer-MiningGame/Tags/FriendsTags.cs
@@ -0,0 +1,10 @@
+﻿namespace Unlimited_NetworkingServer_MiningGame.Tags
+{
+    /// <summary>
+    ///     Tags for mine plugin
+    /// </summary>
+    public static class FriendsTags
+    {
+        private const ushort Shift = Tags.Mine * Tags.TagsPerPlugin;
+    }
+}
